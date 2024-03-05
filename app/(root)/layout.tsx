c42448e52064ceecee2979ecd2243daa6f4b35bf
somu878 <somumydhili19@gmail.com>
2024-03-05 16:08:36 +0530

    added sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,10 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="root-container">
+      <Sidebar />
       <div className="wrapper">{children}</div>
     </div>
   );
